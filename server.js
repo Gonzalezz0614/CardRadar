5efbbee677d98e6b37ec3ed324db8764ecde25e2
Gonzalezz0614 <gonzalezz0614@gmail.com>
2026-06-29 22:26:29 -0400

    Add custom headers for axios request

--- server.js
+++ server.js
@@ -32,7 +32,18 @@ async function scrapeEbay(query) {
 
   const soldResponse = await axios.get(soldUrl, { headers });
   const $sold = cheerio.load(soldResponse.data);
-  
+  headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'none',
+    'Cache-Control': 'max-age=0',
+  };
   const soldPrices = [];
   $sold('.s-item__price').each((i, el) => {
     const priceText = $sold(el).text().replace(/[^0-9.]/g, '');
